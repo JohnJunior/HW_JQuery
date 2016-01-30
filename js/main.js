1b725eb3d94add80634fd20480e8e533e1e0f3bf
JohnJunior <john.tacker@gmail.com>
2016-01-30 16:40:49 +0200

    add dialog, tooltip, tabs, sliders

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ $(window).load(function(){
         $('#menu-nav-open').toggle('slow');
     });
 
+    //scrolling
+    $('.menu-nav a[href^="#"]').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(target).offset().top - 50}, 800);
+        $('.menu-nav').css("display", "none");
+        return false;
+    });
+
     //accordion
     function close_section() {
         $('.accord-header').removeClass('active');
@@ -22,4 +30,62 @@ $(window).load(function(){
     });
 
     //dialog
+    $('#popup, #hide').on('click', function(){
+        $('#popup').hide();
+    });
+
+    $('#popup-pls').on('click', function(){
+        $('#popup').show();
+    });
+
+    $('.popup-content').on('click', function(e) {
+        e.stopImmediatePropagation();
+    });
+
+    //tabs
+    $('span[data-tab-id^="#tabs-"]').on('click', function(){
+        var tab = $(this).data('tab-id');
+        $('.tab-open').removeClass('tab-open');
+        var on_class = $('.tab-content').hasClass("on");
+        if(on_class) {
+            $('.on').hide().removeClass('on');
+            $(this).addClass('tab-open');
+            $(tab).show().addClass('on');
+        } else {
+            $(this).addClass('tab-open');
+            $(tab).show().addClass('on');
+        }
+    });
+
+    //Tolltips
+    var box = '.tt-box';
+    $('*').mouseover(function (e) {
+        var tt_title = $(this).data('tt-title');
+        if (tt_title) {
+            $(box).text(tt_title);
+            $(box).css("left", event.pageX + 15);
+            $(box).css("top", event.pageY + 15);
+            $(box).fadeIn(300);
+        }
+    });
+    $('*').mouseleave(function () {
+        var tt_title = $(this).data('tt-title');
+        if (tt_title) {
+            $(box).fadeOut(300);
+        }
+    });
+
+    //Flexisliders
+    $('.flexslider.single').flexslider({
+        animation: "slide"
+    });
+
+    $('.flexslider.carousel').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 210,
+        itemMargin: 5,
+        minItems: 2,
+        maxItems: 4
+    });
 });
